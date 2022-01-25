5733c1d70ad68cdc56be6bf5623ffa99a584f318
Johson Rodriguez <sukerjc@gmail.com>
2022-01-24 21:06:44 -0500

    fix

--- src/main/java/com/lordbyron/auth/AuthApplication.java
+++ src/main/java/com/lordbyron/auth/AuthApplication.java
@@ -28,19 +28,19 @@ public class AuthApplication {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner run(UserService userService) {
-//		return args -> {
-//			userService.saveRole(new Role(null, "ROLE_USER"));
-//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
-//
-//			userService.saveUser(new Employee(null, "Johson Rodriguez", "jrodriguez", "asdf1234", new ArrayList<>(),true));
-//			userService.saveUser(new Employee(null, "Luis Flores", "lflores", "asdf1234", new ArrayList<>(),true));
-//
-//			userService.addRoleToUser("jrodriguez", "ROLE_ADMIN");
-//			userService.addRoleToUser("jrodriguez", "ROLE_USER");
-//			userService.addRoleToUser("lflores", "ROLE_USER");
-//		};
-//	}
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+
+			userService.saveUser(new Employee(null, "Johson Rodriguez", "jrodriguez", "asdf1234", new ArrayList<>(),true));
+			userService.saveUser(new Employee(null, "Luis Flores", "lflores", "asdf1234", new ArrayList<>(),true));
+
+			userService.addRoleToUser("jrodriguez", "ROLE_ADMIN");
+			userService.addRoleToUser("jrodriguez", "ROLE_USER");
+			userService.addRoleToUser("lflores", "ROLE_USER");
+		};
+	}
 
 }
