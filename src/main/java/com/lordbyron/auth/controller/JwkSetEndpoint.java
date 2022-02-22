1757e75efe8912f4519cf1a70697272be7a38a5f
@JohsonRodriguez <52184527+JohsonRodriguez@users.noreply.github.com>
2022-02-21 19:45:45 -0500

    Update JwkSetEndpoint.java

--- src/main/java/com/lordbyron/auth/controller/JwkSetEndpoint.java
+++ src/main/java/com/lordbyron/auth/controller/JwkSetEndpoint.java
@@ -2,13 +2,11 @@ package com.lordbyron.auth.controller;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("*")
 public class JwkSetEndpoint {
 
     @Autowired
